68989b4116f2e7afb6143bdca09de1fd1c26099d
Dave <daveg7lee@gmail.com>
2022-01-31 11:58:51 +0900

    Recoil Persist

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle, DefaultTheme } from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
